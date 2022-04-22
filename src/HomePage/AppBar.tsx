60eff8a2653289ffcdfd3360fa76cc5d17a8f782
Jason Alexander <jasonalexander.dev@gmail.com>
2022-04-22 11:57:18 +0100

    Further front page work

--- src/HomePage/AppBar.tsx
+++ src/HomePage/AppBar.tsx
@@ -4,30 +4,82 @@ import {
     AppBar as MuiAppBar,
     Toolbar,
     Typography,
-    Grid
-} from '@mui/material';
+    Grid,
+    Button,
+    Box
+} from '@mui/material'; 
+
 
 export function AppBar() {
 
     return (
         <MuiAppBar 
             position="static"
-            color="primary" 
-            enableColorOnDark
         >
             <Toolbar>
                 <Grid container>
-                    <Grid item sm={1} />
+                    <Grid 
+                        item
+                        xs={false}
+                        sm={1}
+                    />
                     <Grid 
                         item 
+                        xs={12}
                         sm={10}
                         container
+                        justifyContent="space-between"
                     >
-                        <Grid item xs={2}>
-                            <Typography variant="h6" noWrap component="div">Hello world</Typography>
-                        </Grid>
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                gap: 2
+                            }}
+                        >
+                            <Button
+                                onClick={() => {}}
+                            >
+                                <img
+                                    alt="Vulpine Designs home"
+                                    src="./images/logo.png"
+                                    height={47}
+                                />
+                            </Button>
+                            <Box
+                                sx={{ 
+                                    display: 'flex',
+                                    alignItems: 'center'
+                                }}
+                            >
+                                <Button
+                                    onClick={() => {}}
+                                    variant="outlined"
+                                    size='small'
+                                >
+                                    Initiator Set
+                                </Button>
+                            </Box>
+                            <Box
+                                sx={{ 
+                                    display: 'flex',
+                                    alignItems: 'center'
+                                }}
+                            >
+                                <Button
+                                    onClick={() => {}}
+                                    variant="outlined"
+                                    size='small'
+                                >
+                                    Vulpine Home
+                                </Button>
+                            </Box>
+                        </Box>
                     </Grid>
-                    <Grid item sm={1} />
+                    <Grid 
+                        item
+                        xs={false}
+                        sm={1}
+                    />
                 </Grid>
             </Toolbar>
         </MuiAppBar>
